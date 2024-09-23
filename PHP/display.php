6be58829f6b2f9c953beddb832c0a6782de88a92
ashimdhungana07 <ashimdhungana6@gmail.com>
2024-09-23 23:27:33 +0545

    new

--- PHP/display.php
+++ PHP/display.php
@@ -15,12 +15,14 @@ $result2 = mysqli_query($conn, $sql);
 
 // Retrieving all the rows at once as an associative array
 
+echo "<h3> Retrieving All Rows At Once </h3>";
 if (mysqli_num_rows($result1) > 0) {
     $rows = mysqli_fetch_all($result1, MYSQLI_ASSOC);
 
     foreach ($rows as $item) {
-        echo "<br>" . $item['name'];
-        echo "<br>" . $item['age'];
+        echo "<br> Name: " . $item['name'];
+        echo "<br> Age: " . $item['age'];
+        echo "<br>";
     }
 } else {
     echo "0 results";
@@ -30,10 +32,12 @@ echo "<hr>";
 
 // Retrieving one row at a time until all the rows are finished
 
+echo "<h3> Retrieving Rows One By One </h3>";
 if ($result2) {
     while($rows = mysqli_fetch_assoc($result2)){
-        echo "<br>" . $rows['name'];
-        echo "<br>" . $rows['age'];
+        echo "<br> Name: " . $rows['name'];
+        echo "<br> Age: " . $rows['age'];
+        echo "<br>";
     }
 } else {
     echo "0 results";
