9478cfb44a9c6c9599f04232b4d745adecb8d21b
ashimdhungana07 <ashimdhungana6@gmail.com>
2024-09-23 23:18:37 +0545

    new

--- PHP/display.php
+++ PHP/display.php
@@ -6,18 +6,37 @@ $password = "database123";
 $database = "php_practice";
 $conn;
 
-$conn = mysqli_connect($servername,$username,$password,$database);
+$conn = mysqli_connect($servername, $username, $password, $database);
 $sql = "SELECT * FROM `Student`";
 
-$result = mysqli_query($conn,$sql);
+$result1 = mysqli_query($conn, $sql);
+$result2 = mysqli_query($conn, $sql);
 
-if (mysqli_num_rows($result) > 0) {
-    $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+// Retrieving all the rows at once as an associative array
+
+if (mysqli_num_rows($result1) > 0) {
+    $rows = mysqli_fetch_all($result1, MYSQLI_ASSOC);
+
+    foreach ($rows as $item) {
+        echo "<br>" . $item['name'];
+        echo "<br>" . $item['age'];
+    }
+} else {
+    echo "0 results";
 }
 
-foreach($rows as $item){
-    echo "<br>" . $item['name'];
-    echo "<br>" . $item['age'];
+echo "<hr>";
+
+// Retrieving one row at a time until all the rows are finished
+
+if ($result2) {
+    while($rows = mysqli_fetch_assoc($result2)){
+        echo "<br>" . $rows['name'];
+        echo "<br>" . $rows['age'];
+    }
+} else {
+    echo "0 results";
 }
 
 ?>
