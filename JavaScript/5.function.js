6a498b792bab768efc42834f392f46856b38b83a
ashimdhungana07 <ashimdhungana6@gmail.com>
2024-09-25 13:11:47 +0545

    new

---
+++ JavaScript/5.function.js
@@ -0,0 +1,11 @@
+// Normal function
+function add(a,b){
+    return a+b;
+}
+let sum = add(4,3);
+console.log("The sum is " + sum);
+
+// Arrow function
+let subtract = (a,b) => a-b;
+let difference = subtract(7,3);
+console.log("The difference is " + difference);
