f320f04f8da57eda2fd1d28b5f7fa1ae38bbf52d
ashimdhungana07 <ashimdhungana6@gmail.com>
2024-10-22 12:23:12 +0545

    new

--- JavaScript/Express JS/main.js
+++ JavaScript/Express JS/main.js
@@ -8,6 +8,21 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+// Creating other pages
+app.get('/about', (req,res) => {
+    res.send('This is about page')
+})
+
+// Using parameters
+app.get('/blog/:slug', (req,res) => {
+    res.send(`Hello ${req.params.slug}`)
+
+    // For URL: http://127.0.0.1:3000/blog/new-blog?name=king
+    console.log(req.params)
+    console.log(req.query)
+})
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
